00a4707d1dff969b1bd73a9a48e652d2e53f3033
Aelybs <aminaturarbekova22@gmail.com>
2025-07-29 17:25:32 +0300

    Внесены дополнения и изменения в код

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -14,18 +14,42 @@ class ViewController: UIViewController {
     @IBOutlet weak var decreaseButton: UIButton!
     @IBOutlet weak var counterLabel: UILabel!
     @IBOutlet weak var textLabel: UILabel!
-    
+    var counter = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counterLabel.text = "\(counter)"
+        historyText.text = "История изменений:"
     }
-
-    @IBAction func decreaseButtonTouch(_ sender: Any) {
+    private func timeAndDate() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH-mm-ss"
+        let dateString = dateFormatter.string(from: Date.init())
+        return dateString
+    }
+    @IBAction private func decreaseButtonTouch(_ sender: Any) {
+        if counter > 0 {
+                   counter -= 1
+                   counterLabel.text = "\(counter)"
+                   historyText.text = "\(timeAndDate()): значение изменено на -1 "
+                   
+               } else if  counter == 0 {
+                   counter = 0
+                   counterLabel.text = "\(counter)"
+                   historyText.text = "\(timeAndDate()): попытка уменьшить значение счётчика ниже 0"
+               }
+    }
+    @IBAction private func increaseButtonTouch(_ sender: Any) {
+        counter += 1
+        counterLabel.text = "\(counter)"
+        historyText.text = "\(timeAndDate()): значение изменено на +1"
+        
+    }
+    @IBAction private  func goBackButtonTouch(_ sender: Any) {
+        counter = 0
+        counterLabel.text = "\(counter)"
+        historyText.text = "\(timeAndDate()): значение сброшено"
     }
     
-    @IBAction func increaseButtonTouch(_ sender: Any) {
-    }
-    @IBAction func goBackButtonTouch(_ sender: Any) {
-    }
 }
-
+    
